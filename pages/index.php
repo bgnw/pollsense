236c10d6c9298a2bca1d2a5c4a246680d1f80598
bgnw <48106124+bgnw@users.noreply.github.com>
2020-02-22 13:51:06 +0000

    Stylistic change: removed ellipses from helptext.

--- pages/index.php
+++ pages/index.php
@@ -21,25 +21,25 @@
                 <div class="card welcome-card">
                     <h3>Join a poll</h3>
                     <p>Give your in opinion in an existing poll, if you know the ID.</p>
-                    <a class="action" href="join" title="Participate in an existing poll...">Join Poll</a>
+                    <a class="action" href="join" title="Participate in an existing poll">Join Poll</a>
                 </div>
                 <div class="card welcome-card">
                     <h3>Create a poll</h3>
                     <p>Create a poll to gather insight on a question from others.</p>
-                    <a class="action" href="create" title="Make a new poll...">Create Poll</a>
+                    <a class="action" href="create" title="Make a new poll">Create Poll</a>
                 </div>
             </div>
             <div class="card-container">
                 <div class="card welcome-card" >
                     <h3>About PollSense</h3>
                     <p>Find out more about this website, and how to contact the developer.</p>
-                    <a class="action secondary" href="about" title="Go to the About page...">About</a>
+                    <a class="action secondary" href="about" title="Go to the About page">About</a>
                 </div>
                 <div class="card welcome-card" >
                     <h3>Sign&nbsp;Up&nbsp;/ Log&nbsp;In</h3>
                     <p>Use an account to see all of your polls in one place, and edit them.</p>
 
-                    <a class="action secondary" href="../scripts/account_redirect.php" title="Make or log into an account...">Account</a>
+                    <a class="action secondary" href="../scripts/account_redirect.php" title="Make or log into an account">Account</a>
                 </div>
             </div>
         </div>
