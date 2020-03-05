599d3a67c18908191c330b2474d97ca7e1eca753
bgnw <48106124+bgnw@users.noreply.github.com>
2020-03-05 09:37:59 +0000

    Minor formatting changes.

--- pages/delete_account.php
+++ pages/delete_account.php
@@ -12,18 +12,24 @@ header("location: ../pages/info?error=no_login");
 }
 ?>
 
-        <!-- Main content -->
-        <div class="content">
-            <div class="card-container">
-                <div class="card">
-                    <form name="delete_account" action="../scripts/form_handler.php" method="post">
-                        <h2>Delete My Data</h2>
-                        <p>This will erase all account and poll data related to the account with username <b><?php echo $_SESSION["username"];?></b>.<br>This action cannot be undone!</h3><br><br>
-                        <input type="checkbox" id="delete_account_confirm" name="delete_account_confirm">
-                        <label for="delete_account_confirm">I wish to completely erase my account.</label><br>
-                        <input class="danger" type="submit" name="delete_account" value="DELETE ACCOUNT">
-                    </form>
-                </div>
+<body id="delete_account">
+    <!-- Navigation bar -->
+    <?php include "../scripts/incl_navbar.php";?>
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="card-container">
+            <div class="card">
+                <form name="delete_account" action="../scripts/form_handler.php" method="post">
+                    <h2>Delete My Data</h2>
+                    <p>This will erase all account and poll data related to this account? (Username: <b><?php echo $_SESSION["username"];?></b>)<br>This action cannot be undone!</h3><br><br>
+                    <input type="checkbox" id="delete_account_confirm" name="delete_account_confirm">
+                    <label for="delete_account_confirm">I wish to completely erase my account.</label><br>
+                    <table><div>
+                        <td><input class="action cancel" type="submit" name="delete_account_cancel" value="Cancel"></td>
+                        <td><input class="danger" type="submit" name="delete_account_submit" value="DELETE ACCOUNT"></td>
+                    </table>
+                </form>
             </div>
         </div>
     </div>
